ed364fa0fa4d70a7abbb5a4289e6cdaed5aa18d4
jwallace22 <47610227+jwallace22@users.noreply.github.com>
2019-12-06 12:09:11 -0600

    added comments

--- hashTable.h
+++ hashTable.h
@@ -6,10 +6,12 @@
 #define PROGRAM_8_HASHTABLE_H
 #include "chunk.h"
 using namespace std;
+//this is the list node that is used in the hashTable to store file names based on their chunks' keys from the hash function
 struct ListNode{
     string fileName;
     ListNode * next;
 };
+//This is ussed as a linked list node specifically for printing out a sorted list of file combinations with more than a given number of similarities
 struct cheaters{
     string file1;
     string file2;
@@ -18,8 +20,8 @@ struct cheaters{
 };
 class hashTable {
 private:
-    static const unsigned int tableSize = 499979;
-    ListNode * myTable[tableSize];
+    static const unsigned int tableSize = 499979;//large prime number chosen because bigger numbers caused valgrind errors
+    ListNode * myTable[tableSize];//the hash table is a table of pointers to our linked lists
 public:
     hashTable();
     void hash(string word,string fileName);
