af06e02f203acfbd4177ff3fbe45bbb955152190
ddnguyen18 <ddnguyen18@mail.strakejesuit.org>
2019-12-06 12:33:22 -0600

    Update hashTable.h

--- hashTable.h
+++ hashTable.h
@@ -24,7 +24,24 @@ private:
     ListNode * myTable[tableSize];//the hash table is a table of pointers to our linked lists
 public:
     hashTable();
+    
+    /*
+    *   Precondition: a hash table has been created
+    *   Input: work chunk of length n, name of file chunk is from    
+    *   Output: none
+    *   Postcondition: word chunk has been hashed, and inputed into the hash table with the file index where it came from
+    */
     void hash(string word,string fileName);
+    
+    /*
+    *   Precondition:a hash table has been created
+    *   Input: vector of file names to be read, threshold for plagerism
+    *   Output: prints file names that are similar
+    *   Postcondition: 
+    *   
+    *   Description: This is the main part of the program as it created the 2D vector that shows the similarities between each combination of files
+    *   and then prints to the screen those files whose similarities are greater than the plagiarism threshold.  
+    */
     void checkCollisions(vector <string> _files, int plagiarismThreshold);
     ~hashTable();
 };
